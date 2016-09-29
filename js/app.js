4130d553005ceff26a187e0132066875ac4fa598
Lee Wilson <leerwilson@gmail.com>
2016-09-29 15:20:50 -0400

    asdf

--- js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('this is after the dom loaded');
-
-  var $listForm = document.querySelector('.list-maker-form');
-  
-});
-
-console.log('this is before the dom loaded');
