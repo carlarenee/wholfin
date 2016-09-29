c64543288333b7afb6b20c340dfbf43cb9fcc4ca
Sang Min Na <xsangmin@gmail.com>
2016-09-29 15:47:05 -0400

    add border when hovering

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('this is after the dom loaded');
 
-  var $listForm = document.querySelector('.list-maker-form');
+  // var $listForm = document.querySelector('.list-maker-form');
+
+  // jquery
+
+  // show border when hovering over individual columns
+  var $columns = $('.columns');
+  $columns.hover(function() {
+    $(this).css('border', '2px solid black');
+  }, function() {
+    $(this).css('border', 'none');
+  });
+
   
+
 });
 
 console.log('this is before the dom loaded');
