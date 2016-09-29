98615f5f3220ed96df71c2f874c8f5a52ac57fed
carla stockalper <cstockalper@gmail.com>
2016-09-29 15:47:49 -0400

    create new text box

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log('this is after the dom loaded');
 
   var $listForm = document.querySelector('.list-maker-form');
-  
+
 });
 
 console.log('this is before the dom loaded');
